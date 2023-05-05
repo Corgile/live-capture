ad03d5b6d38486c70125d3aecd4bee49dcfa33e0
Corgile <mashiro-e@qq.com>
2023-05-05 08:11:47 +0000

    stash

---
+++ example/every-ic.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <thread>
+#include <pcap.h>
+#include <netinet/in.h>
+#include <netinet/if_ether.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
+#include <netinet/ip_icmp.h>
+
+struct PacketData {
+    std::string dev_name;
+    const struct pcap_pkthdr* header;
+    const u_char* packet;
+};
+
+void packet_handler(u_char* arg, const struct pcap_pkthdr* header, const u_char* packet) {
+    std::cout << "void packet_handler(u_char* arg, const struct pcap_pkthdr* header, const u_char* packet)\n";
+    PacketData* data = (PacketData*)arg;
+    std::cout << "DEVICE: " << data->dev_name
+    << " Timestamp: " << header->ts.tv_sec
+    << "." << header->ts.tv_usec << "\n";
+
+    struct iphdr* ip = (struct iphdr*)(packet + sizeof(struct ethhdr));
+
+    std::cout << "struct iphdr* ip = (struct iphdr*)(packet + sizeof(struct ethhdr));";
+    if(ip->protocol != IPPROTO_TCP && ip->protocol != IPPROTO_UDP && ip->protocol != IPPROTO_ICMP){
+        return;
+    }
+
+    switch (ip->protocol) {
+        case IPPROTO_TCP: {
+            struct tcphdr* tcp = (struct tcphdr*)(packet + sizeof(struct ethhdr) + sizeof(struct iphdr));
+            std::cout << " TCP : " << inet_ntoa(*(in_addr*)&ip->saddr) << ":" << ntohs(tcp->source);
+            std::cout << "->" << inet_ntoa(*(in_addr*)&ip->daddr) << ":" << ntohs(tcp->dest) << "\n";
+            break;
+        }
+        case IPPROTO_UDP: {
+            struct udphdr* udp = (struct udphdr*)(packet + sizeof(struct ethhdr) + sizeof(struct iphdr));
+            std::cout << " UDP : " << inet_ntoa(*(in_addr*)&ip->saddr) << ":" << ntohs(udp->source);
+            std::cout << "->" << inet_ntoa(*(in_addr*)&ip->daddr) << ":" << ntohs(udp->dest) << "\n";
+            break;
+        }
+        case IPPROTO_ICMP: {
+            struct icmphdr* icmp = (struct icmphdr*)(packet + sizeof(struct ethhdr) + sizeof(struct iphdr));
+            std::cout << "ICMP("<<(int)icmp->type<<"): " << inet_ntoa(*(in_addr*)&ip->saddr);
+            std::cout << "->" << inet_ntoa(*(in_addr*)&ip->daddr) << "\n";
+            break;
+        }
+        default:
+            std::cout << "currently not supported protocol" << "\n";
+            break;
+    }
+}
+
+void capture_thread(std::string& dev_name) {
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t* device = pcap_create(dev_name.c_str(), errbuf);
+    if (device == NULL) {
+        std::cerr << "Error creating handle for device " << dev_name << ": " << errbuf << "\n";
+        return;
+    }
+
+    int ret = pcap_activate(device);
+    if (ret != 0) {
+        std::cerr << "Error activating handle for device " << dev_name << ": " << pcap_statustostr(ret) << "\n";
+        pcap_close(device);
+        return;
+    }
+
+//    std::cout << "Capturing on device " << dev_name << "\n";
+    PacketData data = {dev_name, nullptr, nullptr};
+    pcap_loop(device, 0, packet_handler, (u_char*)&data);
+    pcap_close(device);
+}
+
+int main() {
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_if_t* devices;
+
+    if (pcap_findalldevs(&devices, errbuf) != 0) {
+        std::cerr << "Error finding devices: " << errbuf << "\n";
+        return 1;
+    }
+
+    for (pcap_if_t* device = devices; device != NULL; device = device->next) {
+        std::string dev_name(device->name);
+       std::cout << "Starting capture on device " << dev_name << "\n";
+        std::thread capture(capture_thread, std::ref(dev_name));
+        capture.detach();
+    }
+
+//    pcap_freealldevs(devices);
+
+//    std::cin.get();
+    return 0;
+}
---
+++ example/test.cpp
@@ -0,0 +1,125 @@
+//demo
+#include <pcap.h>
+#include <pthread.h>
+#include "stdlib.h"
+
+#include "unistd.h"
+#define SNAP_LEN 65535
+
+/* prototype of the packet handler */
+void dispatcher_handler(u_char *temp1, const struct pcap_pkthdr *header, const u_char *pkt_data);
+void *thr_fn1(void *arg);
+void *thr_fn2(void *arg);
+void *thr_fn3(void *arg);
+
+pcap_t *handle1, *handle2;                /* packet capture handle */
+int CAP_TIME = 600;
+pthread_t t1, t2, t3;
+
+int main(int argc, char **argv)
+{
+    char *dev1 = NULL;
+    char *dev2 = NULL;
+    char errbuf[PCAP_ERRBUF_SIZE];        /* error buffer */
+    pcap_if_t *alldev, *p;
+    char filter_exp[] = "tcp";        /* filter expression [3] */
+    struct bpf_program fp1,fp2;            /* compiled filter program (expression) */
+    bpf_u_int32 mask;            /* subnet mask */
+    bpf_u_int32 net;            /* ip */
+    /* check for capture device name on command-line */
+    if (argc == 3) {    //pre-define the two device's names to be captured
+        dev1 = argv[1];
+        dev2 = argv[2];
+    }
+    else if (argc > 3 || argc == 2) {
+        fprintf(stderr, "error: unrecognized command-line options\n\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("please input the capture time\n");
+    scanf("%d",&CAP_TIME);
+    /* print capture info */
+    printf("1st device: %s\n", dev1);
+    /* open capture device */
+    handle1 = pcap_open_live(dev1, SNAP_LEN, 1, 1000, errbuf);
+    if (handle1 == NULL) {
+        fprintf(stderr, "Couldn't open device %s: %s\n", dev1, errbuf);
+        exit(EXIT_FAILURE);
+    }
+    /* make sure we're capturing on an Ethernet device [2] */
+    if (pcap_datalink(handle1) != DLT_EN10MB) {
+        fprintf(stderr, "%s is not an Ethernet\n", dev1);
+        exit(EXIT_FAILURE);
+    }
+    /* compile the filter expression */
+    if (pcap_compile(handle1, &fp1, filter_exp, 0, 24) == -1) {
+        fprintf(stderr, "Couldn't parse filter %s: %s\n",
+                filter_exp, pcap_geterr(handle1));
+        exit(EXIT_FAILURE);
+    }
+    /* apply the compiled filter */
+    if (pcap_setfilter(handle1, &fp1) == -1) {
+        fprintf(stderr, "Couldn't install filter %s: %s\n",
+                filter_exp, pcap_geterr(handle1));
+        exit(EXIT_FAILURE);
+    }
+    /* print capture info */
+    printf("2nd device: %s\n", dev2);
+    printf("Filter expression: %s\n", filter_exp);
+    printf("Caputre time: %d\n", CAP_TIME);
+    /* open capture device */
+    handle2 = pcap_open_live(dev2, SNAP_LEN, 1, 1000, errbuf);
+    if (handle2 == NULL) {
+        fprintf(stderr, "Couldn't open device %s: %s\n", dev2, errbuf);
+        exit(EXIT_FAILURE);
+    }
+    /* make sure we're capturing on an Ethernet device [2] */
+    if (pcap_datalink(handle2) != DLT_EN10MB) {
+        fprintf(stderr, "%s is not an Ethernet\n", dev2);
+        exit(EXIT_FAILURE);
+    }
+    /* compile the filter expression */
+    if (pcap_compile(handle2, &fp2, filter_exp, 0, 24) == -1) {
+        fprintf(stderr, "Couldn't parse filter %s: %s\n",
+                filter_exp, pcap_geterr(handle2));
+        exit(EXIT_FAILURE);
+    }
+    /* apply the compiled filter */
+    if (pcap_setfilter(handle2, &fp2) == -1) {
+        fprintf(stderr, "Couldn't install filter %s: %s\n",
+                filter_exp, pcap_geterr(handle2));
+        exit(EXIT_FAILURE);
+    }
+    pthread_create(&t1, NULL, thr_fn1, NULL);
+    pthread_create(&t2, NULL, thr_fn2, NULL);
+    pthread_create(&t3, NULL, thr_fn3, NULL);
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+    pthread_join(t3, NULL);
+    pcap_freecode(&fp1);
+    pcap_freecode(&fp2);
+    pcap_close(handle1);
+    pcap_close(handle2);
+    printf("\nCapture complete.\n");
+    return 0;
+}
+void dispatcher_handler(u_char *temp1,
+                        const struct pcap_pkthdr *header, const u_char *pkt_data)
+{
+    printf("I get one packet!\n");
+}
+void *thr_fn1(void *arg)
+{
+    pcap_loop(handle1, 0, dispatcher_handler, NULL);
+    //pthread_cancel( t2 );
+}
+void *thr_fn2(void *arg)
+{
+    pcap_loop(handle2, 0, dispatcher_handler, NULL);
+    //pthread_cancel( t1 );
+}
+void *thr_fn3(void *arg)
+{
+    sleep(CAP_TIME);
+    pthread_cancel( t1 );
+    pthread_cancel( t2 );
+}
